2a2f8e3dd3e09a511291bd01227f53524b7d5aa7
Dr. Auxin <paul.torrillo@gmail.com>
2017-01-23 10:33:59 -0500

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,170 @@
 package com.company;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	
+	Scanner user = new Scanner(System.in);
+        int names[144] = {"
+            Black
+                    Navy
+            DarkBlue
+                    MediumBlue
+            Blue
+                    DarkGreen
+            Green
+                    Teal
+            DarkCyan
+                    DeepSkyBlue
+            DarkTurquoise
+                    MediumSpringGreen
+            Lime
+                    SpringGreen
+            Aqua
+                    Cyan
+            MidnightBlue
+                    DodgerBlue
+            LightSeaGreen
+                    ForestGreen
+            SeaGreen
+                    DarkSlateGray
+            DarkSlateGrey
+                    LimeGreen
+            MediumSeaGreen
+                    Turquoise
+            RoyalBlue
+                    SteelBlue
+            DarkSlateBlue
+                    MediumTurquoise
+            Indigo
+                    DarkOliveGreen
+            CadetBlue
+                    CornflowerBlue
+            RebeccaPurple
+                    MediumAquaMarine
+            DimGray
+                    DimGrey
+            SlateBlue
+                    OliveDrab
+            SlateGray
+                    SlateGrey
+            LightSlateGray
+                    LightSlateGrey
+            MediumSlateBlue
+                    LawnGreen
+            Chartreuse
+                    Aquamarine
+            Maroon
+                    Purple
+            Olive
+                    Gray
+            Grey
+                    SkyBlue
+            LightSkyBlue
+                    BlueViolet
+            DarkRed
+                    DarkMagenta
+            SaddleBrown
+                    DarkSeaGreen
+            LightGreen
+                    MediumPurple
+            DarkViolet
+                    PaleGreen
+            DarkOrchid
+                    YellowGreen
+            Sienna
+                    Brown
+            DarkGray
+                    DarkGrey
+            LightBlue
+                    GreenYellow
+            PaleTurquoise
+                    LightSteelBlue
+            PowderBlue
+                    FireBrick
+            DarkGoldenRod
+                    MediumOrchid
+            RosyBrown
+                    DarkKhaki
+            Silver
+                    MediumVioletRed
+            IndianRed
+                    Peru
+            Chocolate
+                    Tan
+            LightGray
+                    LightGrey
+            Thistle
+                    Orchid
+            GoldenRod
+                    PaleVioletRed
+            Crimson
+                    Gainsboro
+            Plum
+                    BurlyWood
+            LightCyan
+                    Lavender
+            DarkSalmon
+                    Violet
+            PaleGoldenRod
+                    LightCoral
+            Khaki
+                    AliceBlue
+            HoneyDew
+                    Azure
+            SandyBrown
+                    Wheat
+            Beige
+                    WhiteSmoke
+            MintCream
+                    GhostWhite
+            Salmon
+                    AntiqueWhite
+            Linen
+                    LightGoldenRodYellow
+            OldLace
+                    Red
+            Fuchsia
+                    Magenta
+            DeepPink
+                    OrangeRed
+            Tomato
+                    HotPink
+            Coral
+                    DarkOrange
+            LightSalmon
+                    Orange
+            LightPink
+                    Pink
+            Gold
+                    PeachPuff
+            NavajoWhite
+                    Moccasin
+            Bisque
+                    MistyRose
+            BlanchedAlmond
+                    PapayaWhip
+            LavenderBlush
+                    SeaShell
+            Cornsilk
+                    LemonChiffon
+            FloralWhite
+                    Snow
+            Yellow
+                    LightYellow
+            Ivory
+                    White"
+        }
+        System.out.println("Enter a color in hexodecimal format, as a RGB triplet, or as a word");
+        String color = user.nextLine();
+        if (color.contains(","))
+        {
+
+        }
+        else
+        {}
+        if
+
+
+
     }
 }
